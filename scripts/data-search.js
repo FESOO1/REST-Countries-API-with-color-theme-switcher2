2eb87d2ffae4181f1879a446e0acd9f2ceeca602
FES001 <firdavs7794948@gmail.com>
2024-12-11 21:55:54 +0500

    Working on it.

--- scripts/data-search.js
+++ scripts/data-search.js
@@ -24,6 +24,31 @@ async function uploadingData() {
                 </div>
             </a>
         `;
+
+        
+
+        // OUTPUT ITSELF
+        const outputItself = document.querySelectorAll('.home-output-itself');
+        const outputItselfName = document.querySelectorAll('.home-output-itself-information-name');
+
+        for (let i = 0; i < outputContainer.childElementCount; i++) {
+            outputItself[i].addEventListener('mouseenter', savingOutputInformation);
+            async function savingOutputInformation() {
+                const response = await fetch('https://restcountries.com/v3.1/name/' + outputItselfName[i].textContent);
+                const countryData = await response.json();
+
+                console.log(countryData[0]);
+                localStorage.setItem('countryFlag', countryData[0].flags.png);
+                localStorage.setItem('countryFlagAlt', countryData[0].flags.alt);
+                localStorage.setItem('countryCommonName', countryData[0].name.common);
+                localStorage.setItem('countryNativeName', countryData[0].name.official);
+                localStorage.setItem('countryPopulation', countryData[0].population);
+                localStorage.setItem('countryRegion', countryData[0].region);
+                localStorage.setItem('countryCapital', countryData[0].capital);
+                localStorage.setItem('countryTopLevelDomain', countryData[0].tld[0]);
+                localStorage.setItem('countryCurrencies', countryData[0].currencies[0].name);
+            };
+        };
     };
 };
 
